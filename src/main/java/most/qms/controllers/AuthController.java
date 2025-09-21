932882f3b38ce8a0bc219a313bad46cf05c3195b
B-Yahia <yahiabahhous@gmail.com>
2025-09-21 12:48:04 +0300

    add service to send sms locally in estonia

--- src/main/java/most/qms/controllers/AuthController.java
+++ src/main/java/most/qms/controllers/AuthController.java
@@ -3,6 +3,7 @@ package most.qms.controllers;
 import most.qms.dtos.requests.LoginRequest;
 import most.qms.dtos.requests.UserRequest;
 import most.qms.dtos.responses.UserResponse;
+import most.qms.services.SMSSenderService;
 import most.qms.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,10 +16,12 @@ import static org.springframework.http.ResponseEntity.status;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final UserService userService;
+    private final SMSSenderService smsSenderService;
 
     @Autowired
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, SMSSenderService smsSenderService) {
         this.userService = userService;
+        this.smsSenderService= smsSenderService;
     }
 
     @GetMapping("/users/{userId}")
@@ -33,6 +36,7 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest login) {
+        smsSenderService.SendSMS("+37258087319","hey you, lol");
         return status(FORBIDDEN).build();
     }
 
---
+++ src/main/java/most/qms/services/SMSSenderService.java
@@ -0,0 +1,22 @@
+package most.qms.services;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SMSSenderService {
+    @Value("${twilio.account.sid}")
+    private String accountSid;
+
+    @Value("${twilio.auth.token}")
+    private String authToken;
+
+    public void SendSMS( String number, String msg){
+        Twilio.init(accountSid,authToken);
+        Message message = Message.creator(new PhoneNumber(number),new PhoneNumber("+18102558787"),msg).create();
+        var Body = message.getBody();
+    }
+}
